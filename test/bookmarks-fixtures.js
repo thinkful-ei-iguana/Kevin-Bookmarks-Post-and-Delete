ac5640976047d2f9b658087f244d1cb307f61f2a
Kevin Moore <moore.kevin.p@gmail.com>
2019-12-13 10:59:21 -0500

    added malicious bookmarks fixture

--- test/bookmarks-fixtures.js
+++ test/bookmarks-fixtures.js
@@ -23,4 +23,22 @@ function makeBookmarksArray() {
   ];
 }
 
-module.exports = { makeBookmarksArray }
+function makeMaliciousBookmark() {
+  const maliciousBookmark = {
+    id: 911,
+    title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    url: 'https://www.hackers.com',
+    description: 'Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.'
+  };
+  const expectedBookmark = {
+    ...maliciousBookmark,
+    title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+    description: 'Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.'
+  };
+  return {
+    maliciousBookmark,
+    expectedBookmark,
+  };
+}
+
+module.exports = { makeBookmarksArray, makeMaliciousBookmark };
